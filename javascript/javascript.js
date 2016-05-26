536f583b5f494d7a0863715b408098005370d86c
Tobias Wollter <tobias.wollter@gmail.com>
2016-05-26 16:32:16 +0200

    fuck you hannes men tack ska du ha

---
+++ javascript/javascript.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+var stickyNavTop = $('nav').offset().top;
+ 
+var stickyNav = function(){
+var scrollTop = $(window).scrollTop();
+      
+if (scrollTop > stickyNavTop) { 
+    $('nav').addClass('sticky');
+} else {
+    $('nav').removeClass('sticky'); 
+}
+};
+ 
+stickyNav();
+ 
+$(window).scroll(function() {
+    stickyNav();
+});
+});
+
+//source http://www.hongkiat.com/blog/css-sticky-position/
